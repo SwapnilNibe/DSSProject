f32282e21bd88bc672c8ca213addfe2c4e1e8381
Swapnil Nibe <nibe229@gmail.com>
2018-09-30 03:58:53 -0700

    features added

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import os
+import openpyxl as xl
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -8,11 +9,18 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(270, 210, 111, 21))
+        self.pushButton.setGeometry(QtCore.QRect(270, 210, 111, 31))
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(270, 260, 111, 41))
+        self.pushButton_2.setGeometry(QtCore.QRect(270, 260, 111, 31))
         self.pushButton_2.setObjectName("pushButton_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(190, 150, 401, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.btn = QtWidgets.QPushButton(self.centralwidget)
+        self.btn.setGeometry(QtCore.QRect(60, 150, 75, 23))
+        self.btn.setObjectName("pushButton")
+        self.btn.clicked.connect(self.showDialog)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 690, 21))
@@ -23,7 +31,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        self.pushButton_2.clicked.connect(self.openfile)
+        self.pushButton_2.clicked.connect(self.launchfile)
         self.pushButton.clicked.connect(self.createfile)
     	
     def retranslateUi(self, MainWindow):
@@ -31,16 +39,22 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "Create Excel File"))
         self.pushButton_2.setText(_translate("MainWindow", "Open Excel "))
-	
+        self.lineEdit.setText(_translate("MainWindow", "There is no current directory"))
+        self.btn.setText(_translate("MainWindow", "Edit Directory"))
+        
+    def showDialog(self):
+        text,result = QtWidgets.QInputDialog.getText(None ,'Working Directory','Enter path')
+        if result == True:
+            self.lineEdit.setText(str(text))
+        os.chdir(text.replace("\\","\\\\"))
+            
     def createfile(self):
-        f = open("C:/Users/admin/Desktop/Enterprise Systems/project/Practice/1.xlsx",'w')    	
-        f.close()
+        file = xl.Workbook()
+        file.save("SalesDSS.xlsx")        
+        file.close()
 
-    def openfile(self):
-        os.startfile("C:/Users/admin/Desktop/Enterprise Systems/project/Practice/1.xlsx")    	
-
-    
-    	
+    def launchfile(self):
+        os.startfile("SalesDSS.xlsx")    	
 
 
 if __name__ == "__main__":
